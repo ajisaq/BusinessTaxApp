4aa8e6049b077cd6411052352bd6e119943c85d2
AJISAQ <54847336+ajisaq@users.noreply.github.com>
2019-12-02 10:23:36 +0100

    Update settings.py

--- wis/settings.py
+++ wis/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-$DATABASE_URL.DATABASES["default"].update(dj_database_url.config(conn_max_age=500))
+DATABASES["default"].update(dj_database_url.config(conn_max_age=500))
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
